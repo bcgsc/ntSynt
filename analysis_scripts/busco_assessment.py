945662dc00f3895ddac2f4bcb49c775af244c5e5
lcoombe <lauren.e.coombe@gmail.com>
2023-08-15 16:17:53 -0700

    Updating busco_assessment.py script to better cover more cases

--- analysis_scripts/busco_assessment.py
+++ analysis_scripts/busco_assessment.py
@@ -6,10 +6,12 @@ import argparse
 from collections import namedtuple, defaultdict
 import re
 import itertools
+import sys
 import intervaltree
 
 Busco = namedtuple("Busco", ["id", "strand", "ms_score", "base_id"])
-BuscoList = namedtuple("BuscoList", ["buscos", "strand", "start_reserve", "end_reserve"])
+BuscoList = namedtuple("BuscoList", ["buscos", "strand", "start_reserve", "end_reserve", "assembly"])
+BlockIdBuscoList = namedtuple("BlockIdBuscoList", ["block_id", "busco_list"])
 
 # Matching same BUSCO, potentially different representative sequences
 busco_re = re.compile(r'^(\S+)_\d+$')
@@ -28,7 +30,7 @@ def read_mappings(assembly: str, busco_fin: str, busco_index: dict) -> set:
                 busco_index[assembly][chrom] = intervaltree.IntervalTree()
             busco_base = re.search(busco_re, busco_id).group(1) if re.search(busco_re, busco_id) else busco_id
             busco_index[assembly][chrom][start:end] = Busco(busco_id, strand, int(ms_score), busco_base)
-            stored_buscos.add(busco_id)
+            stored_buscos.add(busco_base)
     return stored_buscos
 
 
@@ -39,27 +41,26 @@ def get_list_of_buscos(busco_index: dict, assembly: str, chrom: str, start: int,
     start_reserve = [] # Extend the coordinates if needed - accounting for mappings near block edges
     end_reserve = []
     for busco in sorted(busco_index[assembly][chrom].envelop(start, end), key=lambda x: (x.begin, x.end)):
-        if busco.data.id in common_buscos:
+        if busco.data.base_id in common_buscos:
             buscos.append(busco.data)
-    busco_set = set(buscos)
     for busco in sorted(busco_index[assembly][chrom][start: end], key=lambda x: (x.begin, x.end)):
-        if busco.data.id in common_buscos and busco.data.id not in busco_set:
+        if busco.data.base_id in common_buscos:
             if busco.begin <= start: # Overlaps the start
                 start_reserve.append(busco.data)
-            elif busco.end > end:
+            elif busco.end > end: # Overlaps the end
                 end_reserve.append(busco.data)
-    return BuscoList(buscos, strand, start_reserve, end_reserve)
+    return BuscoList(buscos, strand, start_reserve, end_reserve, assembly)
 
 
 def extend_busco_list(buscos: list) -> None:
     "Given the list of BUSCO lists, use the reserves to extend the lists if needed"
-    present_buscos = {busco_id.id for busco_asm_list in buscos for busco_id in busco_asm_list.buscos}
+    present_buscos = {busco_id.base_id for busco_asm_list in buscos for busco_id in busco_asm_list.buscos}
     for busco_asm_list in buscos:
         for busco in reversed(busco_asm_list.start_reserve):
-            if busco.id in present_buscos:
+            if busco.base_id in present_buscos:
                 busco_asm_list.buscos.insert(0, busco)
         for busco in busco_asm_list.end_reserve:
-            if busco.id in present_buscos:
+            if busco.base_id in present_buscos:
                 busco_asm_list.buscos.append(busco)
 
 
@@ -74,51 +75,95 @@ def merge_representative(buscos: list) -> list:
             best_busco = sorted(list(group_iterator), key=lambda x: x.ms_score, reverse=True)[0]
             out_list.append(best_busco)
         return_buscos.append(BuscoList(out_list, busco_asm_list.strand,
-                                       busco_asm_list.start_reserve, busco_asm_list.end_reserve))
+                                       busco_asm_list.start_reserve, busco_asm_list.end_reserve,
+                                       busco_asm_list.assembly))
     return return_buscos
 
 
-def filter_repeat(buscos_list: list) -> list:
-    "Filter out BUSCOs that (after merging representative) are found more than once per assembly"
-    dup_buscos = set()
-    for busco_asm_list in buscos_list:
-        asm_buscos = set()
-        for busco in busco_asm_list.buscos:
-            if busco.base_id in asm_buscos:
-                dup_buscos.add(busco.base_id)
-            else:
-                asm_buscos.add(busco.base_id)
+def filter_repeat(blocks_list: list) -> list:
+    "Filter out BUSCOs that (after merging representative) are found more than once in a given assembly"
+    dup_buscos = {busco_list.assembly: set() for busco_list in blocks_list[0].busco_list}
+    asm_buscos = {busco_list.assembly: set() for busco_list in blocks_list[0].busco_list}
+    for _, buscos_list in blocks_list:
+        for busco_asm_list in buscos_list:
+            for busco in busco_asm_list.buscos:
+                if busco.base_id in asm_buscos[busco_asm_list.assembly]:
+                    dup_buscos[busco_asm_list.assembly].add(busco.base_id)
+                else:
+                    asm_buscos[busco_asm_list.assembly].add(busco.base_id)
 
     new_list = []
-    for busco_asm_list in buscos_list:
-        new_list.append(BuscoList(buscos=[busco for busco in busco_asm_list.buscos if busco.base_id not in dup_buscos],
-                                  strand=busco_asm_list.strand, start_reserve=busco_asm_list.start_reserve,
-                                  end_reserve=busco_asm_list.end_reserve))
+    dup_buscos = {busco for _, busco_list in dup_buscos.items() for busco in busco_list}
+    for busco_id, buscos_list in blocks_list:
+        new_entry = []
+        for busco_asm_list in buscos_list:
+            new_entry.append(BuscoList(buscos=[busco for busco in busco_asm_list.buscos if \
+                                    busco.base_id not in dup_buscos],
+                                    strand=busco_asm_list.strand, start_reserve=busco_asm_list.start_reserve,
+                                    end_reserve=busco_asm_list.end_reserve, assembly=busco_asm_list.assembly))
+        new_list.append(BlockIdBuscoList(busco_id, new_entry))
     return new_list
 
 
-def check_buscos(buscos_list: list, block_id: str, error_counter: dict) -> None:
+def check_buscos(buscos_list: list) -> list:
     "Given a list of buscos per synteny block, assess if they are consistent"
-    i = 0
     # Try to extend the lists if the start/ends are not the same/compatible
     extend_busco_list(buscos_list)
     buscos_list = merge_representative(buscos_list)
-    buscos_list = filter_repeat(buscos_list)
+    return buscos_list
 
+def print_different_buscos(buscos_list: BuscoList, block_id: int) -> None:
+    "Verbose printing of BUSCOs that differed between within block in assemblies"
+    common_buscos = set.intersection(*[{busco_id_strand.base_id for busco_id_strand in busco.buscos} \
+        for busco in buscos_list])
+    for busco in buscos_list:
+        for my_busco in busco.buscos:
+            if my_busco.base_id not in common_buscos:
+                print(f"{block_id}\tdifferent_busco\t{my_busco.base_id}\t{busco.assembly}", file=sys.stderr)
+
+def print_wrong_order(id_list1: list, id_list2: list, block_id: int) -> None:
+    "Verbose printing of block lists found in different orders"
+    print(f"{block_id}\twrong_order\t{id_list1}\t{id_list2}")
+
+def print_wrong_ori(busco1: Busco, busco2: Busco, block_id: int) -> None:
+    "Verbose printing of BUSCOs from blocks with wrong orientation"
+    print(f"{block_id}\twrong_ori\t{busco1[0].id}{busco1[0].strand}\t{busco1[1]}\t"
+          f"{busco2[0].id}{busco2[0].strand}\t{busco2[1]}")
+
+def assess_buscos(buscos_list: list, block_id: str, error_counter: dict, verbose: bool) -> None:
+    "Assess the BUSCO lists after filtering, etc"
     total_buscos = len(set.intersection(*[{busco_id_strand.base_id for busco_id_strand in busco.buscos} \
         for busco in buscos_list]))
     error_counter["total_buscos_assessed"] += total_buscos
 
+    # Check for synteny blocks where the BUSCO IDs are not the same
     if len({len(busco.buscos) for busco in buscos_list}) != 1:
         error_counter["different_buscos"].add(block_id)
+        if verbose:
+            print_different_buscos(buscos_list, block_id)
         return
 
     num_buscos = len(buscos_list[0].buscos)
     if len(set.union(*[{busco_id_strand.base_id for busco_id_strand in busco.buscos} \
             for busco in buscos_list])) != num_buscos:
         error_counter["different_buscos"].add(block_id)
+        if verbose:
+            print_different_buscos(buscos_list, block_id)
         return
 
+    # Check for synteny blocks where the order of BUSCOs is not the same
+    for j in range(len(buscos_list) - 1):
+        base_ids_1 = [busco_id_strand.base_id for busco_id_strand in buscos_list[j].buscos]
+        base_ids_2 = [busco_id_strand.base_id for busco_id_strand in buscos_list[j+1].buscos]
+        if not (base_ids_1 == base_ids_2 or base_ids_1 == list(reversed(base_ids_2))):
+            # they are truly different orders, not just strand
+            error_counter["wrong_order"].add(block_id)
+            if verbose:
+                print_wrong_order(base_ids_1, base_ids_2, block_id)
+            return
+
+    # Check for synteny blocks with orientation issues
+    i = 0
     while i < len(buscos_list[0].buscos):
         curr_buscos = []
         for assembly_buscos in buscos_list:
@@ -127,20 +172,21 @@ def check_buscos(buscos_list: list, block_id: str, error_counter: dict) -> None:
             else:
                 curr_buscos.append((assembly_buscos.buscos[i], assembly_buscos.strand))
         for busco1, busco2 in zip(curr_buscos, curr_buscos[1:]):
-            if busco1[0].base_id != busco2[0].base_id:
-                error_counter["wrong_order"].add(block_id)
-            elif busco1[0].strand != busco2[0].strand and busco1[1] == busco2[1]:
+            if (busco1[0].base_id != busco2[0].base_id) or \
+                (busco1[0].strand != busco2[0].strand and busco1[1] == busco2[1]) or \
+                (busco1[0].strand == busco2[0].strand and busco1[1] != busco2[1]):
                 error_counter["wrong_ori"].add(block_id)
-            elif busco1[0].strand == busco2[0].strand and busco1[1] != busco2[1]:
-                error_counter["wrong_ori"].add(block_id)
+                if verbose:
+                    print_wrong_ori(busco1, busco2, block_id)
         i += 1
 
 
-def assess_synteny_blocks(synteny_blocks: str, common_buscos: set, busco_idx: dict) -> tuple:
+def assess_synteny_blocks(synteny_blocks: str, common_buscos: set, busco_idx: dict, verbose: bool) -> tuple:
     "Assess the synteny blocks based on the mappings"
     curr_block_id = None
     total_blocks = 0
-    curr_block_buscos = [] # List Busco_list
+    all_blocks = [] # list of BlockIdBuscoList
+    curr_block_buscos = [] # list of Busco_list
     error_counter = {"different_buscos": set(), "wrong_order": set(), "wrong_ori": set(),
                      'total_buscos_assessed': 0}
     with open(synteny_blocks, 'r', encoding="utf-8") as fin:
@@ -152,14 +198,21 @@ def assess_synteny_blocks(synteny_blocks: str, common_buscos: set, busco_idx: di
                                                             common_buscos))
             else:
                 if curr_block_id is not None:
-                    check_buscos(curr_block_buscos, curr_block_id, error_counter)
+                    curr_block_buscos = check_buscos(curr_block_buscos)
+                    all_blocks.append(BlockIdBuscoList(curr_block_id, curr_block_buscos))
                     total_blocks += 1
                 curr_block_id = block_id
                 curr_block_buscos = [get_list_of_buscos(busco_idx, assembly, chrom, start, end, strand,
                                                             common_buscos)]
     if curr_block_id is not None:
-        check_buscos(curr_block_buscos, curr_block_id, error_counter)
+        curr_block_buscos = check_buscos(curr_block_buscos)
+        all_blocks.append(BlockIdBuscoList(curr_block_id, curr_block_buscos))
         total_blocks += 1
+
+    all_blocks = filter_repeat(all_blocks)
+
+    for block_id, busco_list in all_blocks:
+        assess_buscos(busco_list, block_id, error_counter, verbose)
 
     return error_counter, total_blocks
 
@@ -173,6 +226,7 @@ def main():
                         required=True)
     parser.add_argument("-p", "--prefix", help="Prefix of output files", default="ntsynt_busco_assessment",
                         type=str)
+    parser.add_argument("--verbose", help="Verbose output to standard error", action="store_true")
     args = parser.parse_args()
 
     busco_index = defaultdict(dict) # assembly -> chromosome -> IntervalTree (data: Busco)
@@ -186,7 +240,7 @@ def main():
             else:
                 common_proteins = common_proteins.intersection(present_buscos)
 
-    error_counter, total_blocks = assess_synteny_blocks(args.blocks, common_proteins, busco_index)
+    error_counter, total_blocks = assess_synteny_blocks(args.blocks, common_proteins, busco_index, args.verbose)
 
     with open(args.prefix + ".summary.tsv", 'w', encoding="utf-8") as fout:
         fout.write('Total_assessed_buscos\tTotal_blocks\tCorrect_blocks\twrong_orientation\t'\
