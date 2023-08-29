0976a7fb2b29a5314e5320eaa91ffcf00669822b
lcoombe <lauren.e.coombe@gmail.com>
2023-08-29 15:00:56 -0700

    Update tests to be consistent with new --w-rounds default

--- bin/ntSynt.py
+++ bin/ntSynt.py
@@ -25,7 +25,7 @@ def main():
                         required=False)
     parser.add_argument("--merge", help="Multiple of window size used for collinear synteny block merging [3]",
                         default=3, type=int)
-    parser.add_argument("--w_rounds", help="List of window sizes for iterative rounds [100 10 5]",
+    parser.add_argument("--w_rounds", help="List of window sizes for iterative rounds [100 10]",
                         nargs="+", default=[100, 10])
     parser.add_argument("--indel", help="Threshold for indel detection [500]", default=500, type=int)
     parser.add_argument("-n", "--dry-run", help="Print out the commands that will be executed", action="store_true")
