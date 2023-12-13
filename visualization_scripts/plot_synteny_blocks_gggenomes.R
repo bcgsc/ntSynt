8f2d5c79634d200a270016b8b63c822603929561
lcoombe <lauren.e.coombe@gmail.com>
2023-12-13 15:32:23 -0800

    Update plot_synteny_blocks_gggenomes.R

--- visualization_scripts/plot_synteny_blocks_gggenomes.R
+++ visualization_scripts/plot_synteny_blocks_gggenomes.R
@@ -18,7 +18,7 @@ parser$add_argument("-p", "--prefix", help = "Output prefix for PNG image", requ
 args <- parser$parse_args()
 
 
-# Prep sequences, including short name
+# Read in and prepare sequences
 sequences <- read.csv(args$sequences, sep = "\t", header = TRUE)
 
 # https://stackoverflow.com/questions/32378108/using-gtoolsmixedsort-or-alternatives-with-dplyrarrange
@@ -33,13 +33,13 @@ links_ntsynt <- read.csv(args$links,
 links_ntsynt$seq_id <- factor(links_ntsynt$seq_id,
                               levels = mixedsort(unique(links_ntsynt$seq_id)))
 links_ntsynt <- links_ntsynt[mixedorder(links_ntsynt$seq_id), ]
-
 links_ntsynt$seq_id2 <- as.character(links_ntsynt$seq_id2)
-
 links_ntsynt$colour_block <- as.factor(links_ntsynt$colour_block)
 
+# Prepare scale bar data frame
 scale_bar <- tibble(x = c(0), xend = c(args$scale),
                     y = c(0), yend = c(0))
+
 # Infer best units for scale bar
 label <- paste(args$scale, "bp", sep = " ")
 if (args$scale %% 1e9 == 0) {
@@ -50,6 +50,7 @@ if (args$scale %% 1e9 == 0) {
   label <- paste(args$scale / 1e3, "kbp", sep = " ")
 }
 
+# Make the ribbon plot - these layers can be fully customized as needed!
 make_plot <- function(links, sequences, add_scale_bar = FALSE) {
 
   p <-  gggenomes(seqs = sequences, links = links)
@@ -57,7 +58,7 @@ make_plot <- function(links, sequences, add_scale_bar = FALSE) {
     geom_link(aes(fill = colour_block), offset = 0, alpha = 0.5) +
     geom_seq(size = 2, colour = "grey") + # draw contig/chromosome lines
     geom_bin_label(aes(label = bin_id), size = 6, hjust = 0.9) + # label each bin
-    geom_seq_label(aes(label=seq_id), vjust = 1.1, size = 4) + # Can add seq labels if desired
+    geom_seq_label(aes(label = seq_id), vjust = 1.1, size = 4) + # Can add seq labels if desired
     theme(axis.text.x = element_text(size = 25),
           legend.position = "bottom") +
     scale_fill_manual(values = hue_pal()(length(unique(links$seq_id))),
@@ -78,6 +79,7 @@ make_plot <- function(links, sequences, add_scale_bar = FALSE) {
 
 synteny_plot <- make_plot(links_ntsynt, sequences, add_scale_bar = TRUE)
 
+# Save the ribbon plot
 ggsave(paste(args$prefix, ".png", sep = ""), synteny_plot,
        units = "cm", widt = 50, height = 20, bg = "white")
 
