0e540f8efca8fe0e1c52ad9c11fd4cc67ff79d89
lcoombe <lauren.e.coombe@gmail.com>
2024-05-10 11:43:50 -0700

    Lint fixes

--- visualization_scripts/plot_synteny_blocks_gggenomes_ggtree.R
+++ visualization_scripts/plot_synteny_blocks_gggenomes_ggtree.R
@@ -16,11 +16,12 @@ suppressWarnings(suppressPackageStartupMessages(library(gggenomes)))
 parser <- ArgumentParser(description = "Plot the ntSynt synteny blocks and cladogram using gggenomes")
 parser$add_argument("-s", "--sequences", help = "Input sequence lengths TSV", required = TRUE)
 parser$add_argument("-l", "--links", help = "Synteny block links", required = TRUE)
-parser$add_argument("-c", "--painting", help="File with chromosome painting information", required = FALSE)
+parser$add_argument("-c", "--painting", help = "File with chromosome painting information", required = FALSE)
 parser$add_argument("--scale", help = "Length of scale bar in bases (default 1 Gbp)", default = 1e9,
                     required = FALSE, type = "double")
 parser$add_argument("--tree", help = "Newick-formatted cladogram", required = TRUE)
-parser$add_argument("--ratio", help = "Ratio adjustment for labels on left side of the ribbon plot. Increase if the labels are cut-off, decrease to decrease space between ribbon plot and cladogram",
+parser$add_argument("--ratio",
+                    help = "Ratio adjustment for labels on left side of the ribbon plot. Increase if the labels are cut-off, decrease to decrease space between ribbon plot and cladogram",
                     default = 0.1, required = FALSE, type = "double")
 parser$add_argument("-p", "--prefix",
                     help = "Output prefix for PNG image (default synteny_gggenomes_plot)", required = FALSE,
@@ -71,7 +72,7 @@ painting <- read.csv(args$painting, sep = "\t", header = TRUE)
 # Make the ribbon plot - these layers can be fully customized as needed!
 make_plot <- function(links, sequences, painting, add_scale_bar = FALSE) {
   num_colours <- length(unique(links$colour_block))
-  p <-  gggenomes(seqs = sequences, links = links, feats=painting)
+  p <-  gggenomes(seqs = sequences, links = links, feats = painting)
   plot <- p + theme_gggenomes_clean(base_size = 15) +
     geom_link(aes(fill = colour_block), offset = 0, alpha = 0.5, size = 0.05) +
     geom_seq(size = 2, colour = "darkgrey") + # draw contig/chromosome lines
@@ -83,20 +84,20 @@ make_plot <- function(links, sequences, painting, add_scale_bar = FALSE) {
     scale_fill_manual(values = hue_pal()(num_colours),
                       breaks = levels(links_ntsynt$colour_block)) +
     scale_colour_manual(values = hue_pal()(num_colours),
-                      breaks = levels(links_ntsynt$colour_block)) +
+                        breaks = levels(links_ntsynt$colour_block)) +
     guides(fill = guide_legend(title = "", ncol = 10),
            colour = guide_legend(title = ""))
-    xmax <- ggplot_build(plot)$layout$panel_params[[1]]$x.range[[2]]
-    plot <- plot + xlim(0 - xmax*args$ratio, NA)
+  xmax <- ggplot_build(plot)$layout$panel_params[[1]]$x.range[[2]]
+  plot <- plot + xlim(0 - xmax * args$ratio, NA)
 
   if (add_scale_bar) {
     plot <- plot + geom_segment(data = scale_bar, aes(x = x, xend = xend, y = y, yend = yend),
                                 linewidth = 1.5) +
       geom_text(data = scale_bar, aes(x = x + (xend / 2), y = y - 0.3, label = label)) +
-      theme(axis.line.x = element_blank(), 
-           axis.title.x = element_blank(),
-           axis.text.x = element_blank(),
-           axis.ticks.x = element_blank())
+      theme(axis.line.x = element_blank(),
+            axis.title.x = element_blank(),
+            axis.text.x = element_blank(),
+            axis.ticks.x = element_blank())
   }
 
   return(plot)
@@ -113,9 +114,9 @@ ntsynt_ggtree <- ggtree(ntsynt_tree, branch.length = "none")
 # Align the plots properly
 synteny_y_range <- ggplot_build(synteny_plot)$layout$panel_params[[1]]$y.range
 
-plots <- ggarrange(ntsynt_ggtree + scale_y_continuous(limits=synteny_y_range, expand=c(0, 0)), 
+plots <- ggarrange(ntsynt_ggtree + scale_y_continuous(limits = synteny_y_range, expand = c(0, 0)),
                    (synteny_plot %>% pick_by_tree(ntsynt_ggtree)),
-                   common.legend = T, align = "hv",
+                   common.legend = TRUE, align = "hv",
                    widths = c(1, 10), legend = "bottom")
 
 
